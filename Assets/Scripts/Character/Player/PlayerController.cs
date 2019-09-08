13fcd9f1b3e4fb4a294247a05a79fc53a54a5653
PATP4SE <p-a_morin@hotmail.com>
2019-09-08 10:01:23 -0400

    Scene de lighting degeu + autres tests

--- Assets/Scripts/Character/Player/PlayerController.cs
+++ Assets/Scripts/Character/Player/PlayerController.cs
@@ -6,10 +6,16 @@ public class PlayerController : MonoBehaviour {
 
     [SerializeField] private CharacterMovement2D _characterMovement;
     [SerializeField] private Animator _playerAnimator;
+    [SerializeField] private Transform _playerTransform;
+    [SerializeField] private Camera _camera;
 
     private bool _hasAttacked;
     private PlayerMovement _playerMovement;
     private Player _player;
+    private Vector3 _mousePosition1;
+    private Vector3 _mousePosition2;
+    private Vector3 _mousePosition3;
+
 
     private float _horizontalDirection;
     private bool _isJumping;
@@ -31,12 +37,13 @@ public class PlayerController : MonoBehaviour {
 
         if (_horizontalDirection != 0)
         {
-            _playerAnimator.SetBool("isWalking", true);
+            _playerAnimator.SetBool("IsWalking", true);
             Debug.Log("isWalking");
         }    
         else
         {
-            _playerAnimator.SetBool("isWalking", false);
+            Debug.Log("notWalking");
+            _playerAnimator.SetBool("IsWalking", false);
         }
 
         if (Input.GetButtonDown("Jump"))
@@ -58,19 +65,37 @@ public class PlayerController : MonoBehaviour {
             _playerAnimator.SetTrigger("Attack");
         }
 
-        //if (Input.GetKey(KeyCode.A))
-        //{
-        //	playerMovement.MoveLeft ();
-        //}
-        //if (Input.GetKey (KeyCode.D))
-        //{
-        //	playerMovement.MoveRight ();
-        //}
-        //if (Input.GetKeyDown (KeyCode.Space))
-        //{
-        //	playerMovement.Jump ();
-        //}
+        Vector3 mousePosition1 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition1.z = 0;
+        Vector3 mousePosition2 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition2.z = 0;
+        Vector3 mousePosition3 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition3.z = 0;
 
+        _mousePosition1 = mousePosition1;
+
+        //Vector3 direction = new Vector3(
+        //    mousePosition.x - _playerTransform.position.x,
+        //    mousePosition.y - _playerTransform.position.y);
+
+        Debug.Log("Mouse position: " + mousePosition1.x + " x " + mousePosition1.y + " y ");
+
+        Ray ray = new Ray(_playerTransform.position, Input.mousePosition);
+        Debug.DrawRay(_playerTransform.position, mousePosition1 - _playerTransform.position);
+
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(_mousePosition1, 0.1f);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(_mousePosition1, 0.1f);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(_mousePosition1, 0.1f);
     }
 
     private void FixedUpdate()
